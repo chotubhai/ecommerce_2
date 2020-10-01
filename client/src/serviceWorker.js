43a2eac04ca6c0077c5e4cd2ec44cc6072e3b144
ritik singh <mr.ritik99@gmail.com>
2020-10-01 13:12:02 +0530

    Update serviceWorker.js

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -10,11 +10,11 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://bit.ly/CRA-PWA
 
-const is54.144.49.79 = Boolean(
-  window.location.hostname === '54.144.49.79' ||
-    // [::1] is the IPv6 54.144.49.79 address.
+const islocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
-    // 127.0.0.0/8 are considered 54.144.49.79 for IPv4.
+    // 127.0.0.0/8 are considered localhost for IPv4.
     window.location.hostname.match(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
     )
@@ -34,11 +34,11 @@ export function register(config) {
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
-      if (is54.144.49.79) {
-        // This is running on 54.144.49.79. Let's check if a service worker still exists or not.
+      if (islocalhost) {
+        // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
 
-        // Add some additional logging to 54.144.49.79, pointing developers to the
+        // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
           console.log(
@@ -47,7 +47,7 @@ export function register(config) {
           );
         });
       } else {
-        // Is not 54.144.49.79. Just register service worker
+        // Is not localhost. Just register service worker
         registerValidSW(swUrl, config);
       }
     });
